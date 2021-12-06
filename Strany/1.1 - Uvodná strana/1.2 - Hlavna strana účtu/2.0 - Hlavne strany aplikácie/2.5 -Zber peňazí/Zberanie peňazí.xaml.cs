e07b612d86685533654e016c80cf73b6eaff1e7f
Šimon Potočňák <simon.potocnak03@gmail.com>
2021-12-06 10:27:24 +0100

    Vytvorena stránka na zber peňazí

---
+++ Strany/1.1 - Uvodná strana/1.2 - Hlavna strana účtu/2.0 - Hlavne strany aplikácie/2.5 -Zber peňazí/Zberanie peňazí.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Ročňíkový_projekt___Aplikácia_pre_banku.Strany._1._1___Uvodná_strana._1._2___Hlavna_strana_účtu._2._0___Hlavne_strany_aplikácie._2._5__Zber_peňazí
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class Zberanie_peňazí : Page
+    {
+        public Zberanie_peňazí()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
